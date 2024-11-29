f8d677252654a7d02e82edb550625d0694114725
Pavun786 <pulsarpavuns786@gmail.com>
2024-11-29 05:47:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchData from "./SearchData";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import MovieList from "./MovieList";
+import Favorites from "./Favorites";
+import MovieDetail from "./MovieDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/search" element={<SearchData />} />
+        <Route path="/" element={<MovieList />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/searchData" element={<SearchData />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"
+import { MovieProvider } from './Context/FetchAllMovies';
+import { SearchProvider } from './Context/Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <MovieProvider>
+      <SearchProvider>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </SearchProvider>
+    </MovieProvider>
   </React.StrictMode>
 );
 
